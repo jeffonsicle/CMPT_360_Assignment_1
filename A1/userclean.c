4031f0388919b6f6628e859844c798aca1859fd5
Jeffrey Moniz <146683404+jeffonsicle@users.noreply.github.com>
2026-01-07 20:07:44 +0000

    updated c code and makefile test cases

--- A1/userclean.c
+++ A1/userclean.c
@@ -1,13 +1,27 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 
 int main() {
     char buf[256];
 
-    while (fgets(buf, sizeof buf, stdin)) {
-        printf("READ: [%s]", buf);
+    // move input from stdin to buf
+    fgets(buf, sizeof buf, stdin);
+
+    // Find the first occurrence of '\n' or '\r' and replace it with '\0'
+    buf[strcspn(buf, "\n\r")] = '\0';
+
+    // print the buf string followed by " : "
+    printf("%s : ", buf);
+
+    // checks if the buf char is root or admin, if it is print error and exit program
+    if(strcmp(buf, "root") == 0 || strcmp(buf, "admin") == 0) {
+        printf("invalid and unfixable\n");
+        return 0;
     }
 
+    // print a new line character
+    printf("\n");
     return 0;
 
 }
