b4ef5af2d6578c2f55e02d67f862dfaab4b8f4a8
Jeffrey Moniz <146683404+jeffonsicle@users.noreply.github.com>
2026-01-07 19:36:14 +0000

    test update

--- A1/userclean.c
+++ A1/userclean.c
@@ -1,6 +1,13 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 int main() {
-    printf("Hello world");
+    char buf[256];
+
+    while (fgets(buf, sizeof buf, stdin)) {
+        printf("READ: [%s]", buf);
+    }
+
     return 0;
+
 }
